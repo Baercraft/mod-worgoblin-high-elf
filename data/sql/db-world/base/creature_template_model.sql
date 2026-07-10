0fe91427e4071033cefc488564a3bb556b98fe3e
Medviten <medviten@protonmail.com>
2026-07-10 14:22:10 +0200

    Fixed so it deletes all the entries.

--- data/sql/db-world/base/creature_template_model.sql
+++ data/sql/db-world/base/creature_template_model.sql
@@ -1,5 +1,5 @@
 /* Add models for racial mounts and goblin racial bank NPC */
-DELETE FROM `creature_template_model` WHERE `CreatureID` IN (36613, 46754, 46755, 55272, 55273);
+DELETE FROM `creature_template_model` WHERE `CreatureID` IN (36613, 46754, 46755, 55272, 55273, 55274, 55275, 55276, 55277);
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
 	(36613, 0, 32385, 1, 1, 12340), -- Gobber
 	(46754, 0, 35249, 1, 1, 12340), -- Goblin Trike
