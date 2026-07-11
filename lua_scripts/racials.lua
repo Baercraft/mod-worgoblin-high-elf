d2e7cb28db9dc12dcde579509b70f57d43488bc6
Medviten <medviten@protonmail.com>
2026-07-11 19:59:13 +0200

    Trying to fix Worgen racials.

--- lua_scripts/racials.lua
+++ lua_scripts/racials.lua
@@ -1,17 +1,6 @@
-local WORGEN = 12
-local LEVEL = 20
-
-local function OnFirstLogin(event, player)
-    if player:GetLevel() ~= 1 then
-        player:RemoveSpell(87840)
-    end
-end
-
-RegisterPlayerEvent(30, OnFirstLogin)
-
-function OnLevelChange(event, player, oldLevel)
-    if oldLevel < LEVEL and player:GetLevel() >= LEVEL then
-        if player:GetRace() == WORGEN then
+local function OnLevelChange(event, player, oldLevel)
+    if oldLevel < LEVEL and player:GetLevel() >= 20 then
+        if player:GetRace() == 12 then
             if not player:HasSpell(33388) then
                 player:LearnSpell(33388)
             end
