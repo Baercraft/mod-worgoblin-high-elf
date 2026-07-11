6d34503470b159805a83c576e74adc853e6dd7bd
Medviten <medviten@protonmail.com>
2026-07-11 12:34:12 +0200

    Added a script to teach Worgen racials

---
+++ lua_scripts/racials.lua
@@ -0,0 +1,22 @@
+local WORGEN = 3
+local LEVEL = 20
+
+function OnLevelChange(event, player, oldLevel)
+    if oldLevel < LEVEL and player:GetLevel() >= LEVEL then
+        if player:GetRace() == WORGEN then
+            if not player:HasSpell(33388) then
+                player:LearnSpell(33388)
+            end
+
+            if not player:HasSpell(87840) then
+                player:LearnSpell(87840)
+            end
+
+            if not player:HasSpell(68996) then
+                player:LearnSpell(68996)
+            end
+        end
+    end
+end
+
+RegisterPlayerEvent(13, OnLevelChange)
