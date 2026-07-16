63485740c930b628ba97f752fa6a5ecf81fa3996
Medviten <medviten@protonmail.com>
2026-07-16 13:53:16 +0200

    Removed extra spells and added ranks.

---
+++ data/sql/db-world/base/spell_ranks.sql
@@ -0,0 +1,11 @@
+/* Add two ranks for Running Wild */
+REPLACE INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES
+(
+    87840, -- Basic spell ID: Running Wild (rank 1)
+    87840, -- Spell ID: Running Wild (rank 1)
+    1 -- Rank
+), (
+    87840, -- Basic spell ID: Running Wild (rank 1)
+    87839, -- Spell ID: Running Wild (rank 2) 
+    2 -- Rank
+);
