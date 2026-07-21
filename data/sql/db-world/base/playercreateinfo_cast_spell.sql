ded76ba65411f9e8fcd6c2575d7cbb54c97c6be5
Medviten <medviten@protonmail.com>
2026-07-21 22:28:00 +0200

    Added 33391 to playercreateinfo_cast_spell for DK

--- data/sql/db-world/base/playercreateinfo_cast_spell.sql
+++ data/sql/db-world/base/playercreateinfo_cast_spell.sql
@@ -1,4 +1,5 @@
 REPLACE INTO `playercreateinfo_cast_spell` VALUES
 (0,32,48266,'Death Knight - Blood Presence'), -- AC default
 (0,32,33388,'Death Knight - Apprentice Riding'),
+(0,32,33391,'Death Knight - Journeyman Riding'),
 (0,1,2457,'Warrior - Battle Stance'); -- AC default
