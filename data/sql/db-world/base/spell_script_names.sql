0b5cd0549b5b92d0e6a886005951f3ed05908af1
Medviten <medviten@protonmail.com>
2026-08-16 21:32:35 +0200

    Merged the two spell_script_names files

--- data/sql/db-world/base/spell_script_names.sql
+++ data/sql/db-world/base/spell_script_names.sql
@@ -1,3 +1,8 @@
 /* Add goblin rocket barrage racial ability */
 DELETE FROM `spell_script_names` WHERE `spell_id` = 69041;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (69041, 'spell_rocket_barrage');
+
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(68994, 'spell_costume_override'),
+(110020, 'spell_costume_override')
+ON DUPLICATE KEY UPDATE ScriptName = VALUES(ScriptName);
--- data/sql/db-world/extra/spell_script_names.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(68994, 'spell_costume_override'),
-(110020, 'spell_costume_override')
-ON DUPLICATE KEY UPDATE ScriptName = VALUES(ScriptName);
