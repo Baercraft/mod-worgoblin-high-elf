eddbd8ec91d32e91afa9c09019d0d862ef90ebdf
Medviten <medviten@protonmail.com>
2026-08-30 16:40:59 +0200

    Fixed display ID and model collisions. Added indetermined displays.

---
+++ modpaks/F-033_mod-playable-ogres/dbc/[F-033]_creaturedisplayinfo.sql
@@ -0,0 +1,5 @@
+-- creaturedisplayinfo: 0 inserts, 2 updates, 0 deletes
+
+-- New entries
+UPDATE `creaturedisplayinfo` SET `extended_display_info_id` = 45439 WHERE `id` = 62110;
+UPDATE `creaturedisplayinfo` SET `extended_display_info_id` = 45440 WHERE `id` = 62111;
