8ec5c358c70755a6ddbb0fb5b64a6246d6a20b3f
Medviten <medviten@protonmail.com>
2026-08-30 18:14:26 +0200

    Added _factiontemplate entries for custom races

---
+++ modpaks/F-032_mod-maghar/dbc/[F-033]_chrraces.sql
@@ -0,0 +1,6 @@
+-- chrraces: 0 inserts, 1 updates, 0 deletes
+
+SET @MagharOrc = 14;
+
+-- New entries
+UPDATE `chrraces` SET `faction_id` = 2240 WHERE `id` = @MagharOrc;
---
+++ modpaks/F-033_mod-playable-ogres/dbc/[F-033]_chrraces.sql
@@ -0,0 +1,6 @@
+-- chrraces: 0 inserts, 1 updates, 0 deletes
+
+SET @Ogre = 15;
+
+-- New entries
+UPDATE `chrraces` SET `faction_id` = 2241 WHERE `id` = @Ogre;
